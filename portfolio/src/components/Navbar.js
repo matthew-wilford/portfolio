17153cf0f61ff5787feed214647cf4aef5ff22e1
Matthew Wilford <matthew.wilford@outlook.com>
2024-01-14 16:16:21 -0500

    Fixed navbar-toggler functionality on mobile and deleted redundant css

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,38 +1,70 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/styles/navbar.css";
 
 export default function Navbar() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  const handleHomepageClick = () => {
+    setIsNavCollapsed(true);
+  };
+
   return (
     <nav className="navbar bg-secondary shadow py-4 navbar-expand-sm">
-      <Link to="/Website-Portfolio/" className="navbar-brand ps-4">
+      <Link
+        to="/Website-Portfolio/"
+        className="navbar-brand ps-4"
+        onClick={handleHomepageClick}
+      >
         Matthew Wilford
       </Link>
       <button
-        className="navbar-toggler btn-primary pe-4 border-0"
+        className="navbar-toggler custom-toggler pe-4 border-0"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarNav"
         aria-controls="navbarNav"
-        aria-expanded="false"
+        aria-expanded={!isNavCollapsed ? true : false}
         aria-label="Toggle navigation"
+        onClick={handleNavCollapse}
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse px-4" id="navbarNav">
+      <div
+        className={`collapse navbar-collapse px-4 ${
+          isNavCollapsed ? "" : "show"
+        }`}
+        id="navbarNav"
+      >
         <ul className="navbar-nav ms-auto pe-4">
-          <CustomLink to="/Website-Portfolio/about">About</CustomLink>
-          <CustomLink to="/Website-Portfolio/projects">Projects</CustomLink>
-          <CustomLink to="/Website-Portfolio/contact">Contact</CustomLink>
+          <CustomLink
+            to="/Website-Portfolio/about"
+            handleNavCollapse={handleNavCollapse}
+          >
+            About
+          </CustomLink>
+          <CustomLink
+            to="/Website-Portfolio/projects"
+            handleNavCollapse={handleNavCollapse}
+          >
+            Projects
+          </CustomLink>
+          <CustomLink
+            to="/Website-Portfolio/contact"
+            handleNavCollapse={handleNavCollapse}
+          >
+            Contact
+          </CustomLink>
         </ul>
       </div>
     </nav>
   );
 }
 
-function CustomLink({ to, children }) {
+function CustomLink({ to, children, handleNavCollapse }) {
   return (
     <li className="nav-item">
-      <Link to={to} className="nav-link">
+      <Link to={to} className="nav-link" onClick={handleNavCollapse}>
         {children}
       </Link>
     </li>
