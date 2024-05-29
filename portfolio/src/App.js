be81eadee368a5f9f0ed1e4dd3fa4d6627e7951d
Matthew Wilford <matthew.wilford@outlook.com>
2024-05-29 18:18:04 -0400

    Second test for 404 pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,27 +1,34 @@
-import { Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
+import { useEffect } from "react";
+import { Route, Routes, useHistory, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import "./assets/styles/styles.css"
-import "./assets/styles/main.scss"
+import "./assets/styles/styles.css";
+import "./assets/styles/main.scss";
 
 function App() {
+  const location = useLocation();
 
-    return (
-      <div class="background bg-primary">
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/portfolio/" element={<Home />} />
-            <Route path="/portfolio/about" element={<About />} />
-            <Route path="/portfolio/projects" element={<Projects />} />
-            <Route path="/portfolio/contact" element={<Contact />} />
-          </Routes>
-        </div>
+  useEffect(() => {
+    // Store the current page URL in local storage before navigating to a new page
+    localStorage.setItem("previousLocation", location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <div className="background bg-primary">
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/portfolio/" element={<Home />} />
+          <Route path="/portfolio/about" element={<About />} />
+          <Route path="/portfolio/projects" element={<Projects />} />
+          <Route path="/portfolio/contact" element={<Contact />} />
+        </Routes>
       </div>
-    )
+    </div>
+  );
 }
 
 export default App;
